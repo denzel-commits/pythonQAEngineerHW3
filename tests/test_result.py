b4dc287d6c6ec65ca70cb23b1d703f76955b906e
denzel-commits <dguchinsky@yahoo.com>
2023-07-19 16:38:57 +0600

    Doc: add docstring to tests

--- tests/test_result.py
+++ tests/test_result.py
@@ -6,12 +6,14 @@ from src.pydantic_schemas.result_json import User
 
 @pytest.mark.parametrize("result_file_path", [OUTPUT_FILE_PATH+"result.json"])
 def test_result_json(result_file_path):
+    """Throw a ValueError if the file you pass can't be decoded as JSON."""
     with open(result_file_path, "r") as json_file:
         json.load(json_file)
 
 
 @pytest.mark.parametrize("result_file_path", [OUTPUT_FILE_PATH+"result.json"])
 def test_result_structure(result_file_path, get_json):
+    """Validates json structure with pydantic schema"""
     result = get_json(result_file_path)
 
     if isinstance(result, list):
